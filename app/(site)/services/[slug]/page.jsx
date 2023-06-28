657b9f481d2cf5b4849e6dd7dddd4790f41d135e
Mufaddal Aurangabadi <mufaddalaurangabadi@Mufaddals-MacBook-Pro.local>
2023-06-28 11:20:44 +0530

    new

--- app/(site)/services/[slug]/page.jsx
+++ app/(site)/services/[slug]/page.jsx
@@ -1,10 +1,13 @@
+
 import Image from "next/image";
 import Cta from "@components/Cta";
 import PortableText from "react-portable-text";
 import { getServicesData } from "@sanity/sanity-utils";
 
-export default async function ServicesPage({ services, params }) {
-  const { servicesTitle, image, serviceDesc, blockText } = services;
+export default async function ServicesPage({ params }) {
+
+  const services = await getServicesData(params.slug);
+  console.log(services);
 
   return (
     <>
@@ -13,31 +16,32 @@ export default async function ServicesPage({ services, params }) {
           <div className="m-b0">
             <h6>Services</h6>
 
-            <h3 className="fw4 m-b20">{servicesTitle}</h3>
+            <h3 className="fw4 m-b20">{services?.servicesTitle}</h3>
           </div>
           <hr />
           <div className="m-t10 m-b10">
-            {image && (
-              <Image
-                className="radius-md"
-                width={1080}
-                height={500}
-                src={image}
-                alt={servicesTitle}
-              />
-            )}
+            <Image
+              className="radius-md"
+              width={1080}
+              height={500}
+              src={services?.image}
+              alt={services?.servicesTitle}
+            />
           </div>
           <div className="m-t20">
-            {serviceDesc && (
+            {services?.serviceDesc && (
               <PortableText
-                content={serviceDesc}
+                // Pass in block content straight from Sanity.io
+                content={services?.serviceDesc}
                 projectId="jh3jibfq"
                 dataset="production"
+                // Optionally override marks, decorators, blocks, etc. in a flat
+                // structure without doing any gymnastics
               />
             )}
           </div>
           <div className="bg-gray-light p-40 radius-md">
-            <p>{blockText}</p>
+            <p>{services?.blockText}</p>
           </div>
         </div>
       </section>
